06f3996789e309325af71879dc2d9e343fb96674
Sandro Tributi <sandro.tributi@gmail.com>
2023-05-07 11:33:14 +0200

    Add files via upload

---
+++ TL_Test.ino
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+#define REDPIN 2
+#define YELLOWPIN 3
+#define GREENPIN 4
+
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(YELLOWPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  digitalWrite(REDPIN, HIGH);
+  digitalWrite(YELLOWPIN, HIGH);
+  digitalWrite(GREENPIN, HIGH);
+}
+
+void loop() {
+    
+}
